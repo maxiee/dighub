d779fe26a582fde42933822af3976be1f26e1baa
maxiee <maxieewong@gmail.com>
2022-09-29 22:11:09 +0800

    clean main.dart

---
+++ lib/page/page_home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("DigHub")),
+    );
+  }
+}
