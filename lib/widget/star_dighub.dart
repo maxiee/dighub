c78a72e04e6ef40e785b3b6bce96e9819963f592
maxiee <maxieewong@gmail.com>
2022-10-01 08:40:52 +0800

    rateLimit remain

--- lib/widget/star_dighub.dart
+++ lib/widget/star_dighub.dart
@@ -1,3 +1,4 @@
+import 'package:dighub/global.dart';
 import 'package:flutter/material.dart';
 
 class StarDighub extends StatelessWidget {
@@ -6,15 +7,22 @@ class StarDighub extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Icon(Icons.star, color: Colors.yellow),
-          Text(
-            "maxiee/dighub",
-            style: TextStyle(fontSize: 20, color: Colors.yellow),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.star, color: Colors.yellow),
+              Text(
+                "maxiee/dighub",
+                style: TextStyle(fontSize: 20, color: Colors.yellow),
+              ),
+              Icon(Icons.star, color: Colors.yellow),
+            ],
           ),
-          Icon(Icons.star, color: Colors.yellow),
+          if (Global.gitHub.rateLimitRemaining != null)
+            Text('rateLimit remain ${Global.gitHub.rateLimitRemaining}')
         ],
       ),
     );
